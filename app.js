70b5b72fb1477e4b9dd580061f09afa98a8dea8c
unknown <chenmenglou@qq.com>
2015-01-19 18:18:52 +0800

    119

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var express=require('express');
 var bodyParser=require('body-parser');
 var multer=require('multer');
 var app=express();
+app.use(express.static(require('path').join(__dirname,'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(multer());
-app.use(express.static(require('path').join(__dirname,'public')));
 app.set('views',require('path').join(__dirname,'views'));
 app.set('view engine','html');
 app.engine('.html',require('ejs').__express);
@@ -13,7 +13,7 @@ app.get('/',function(req,res){
 	res.render('login');
 });
 app.post('/login',function(req,res){
-	console.log('usenameIs:'+req.body.usename);
+	console.log('usenameIs:'+req.body.username);
 });
 app.listen(3000,function(){
 	console.log('runInPort3000!!');
